0594eb91d0f31c0b795bf2ba7736489fcc44f292
panda-pritam <mondalrumki02@gmail.com>
2024-03-30 02:25:24 +0530

    counter coded

---
+++ class-compo/src/CounterApp.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+// function CounterApp() {
+//   const [count, setCount] = useState(0);
+
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   const decrement = () => {
+//     setCount(count - 1);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Counter App</h1>
+//       <p>Count: {count}</p>
+//       <button onClick={increment}>Increment</button>
+//       <button onClick={decrement}>Decrement</button>
+//     </div>
+//   );
+// }
+
+class CounterApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default CounterApp;
