4aef45cdb915ac6d5758acb1272153e9b2714d35
panda-pritam <mondalrumki02@gmail.com>
2024-03-31 01:59:55 +0530

    some new projects

---
+++ spell-check/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SpellCheckApp from "./SpellCheckApp";
+function App() {
+  return (
+    <div>
+      <SpellCheckApp />
+    </div>
+  );
+}
+
+export default App;
